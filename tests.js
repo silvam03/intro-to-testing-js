ab5fb5a22dc8209ac96b7ee6ae75c5b43c8226e8
Matias Silva <silvamatias03@gmail.com>
2022-10-12 13:54:01 -0500

    add the third test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -18,4 +18,10 @@ describe("sayHello", function(){
     it("should be a defined function", function() {
         expect(typeof sayHello).toBe("function");
     });
+    it("should return a string when called", function() {
+        expect(typeof sayHello()).toBe("string");
+    })
+    it("should return the string 'Hello, Jane!' when executed", function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    })
 })
